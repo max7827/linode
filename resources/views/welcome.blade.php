cb8d7f25f2698e1231a05ff4517b9a703d69ca4f
dau <dau@dau.com>
2020-07-07 21:57:08 +0530

    dsd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Billi
                 </div>
 
                 <div class="links">
